Nyanm/oai-ran-tlab

Separate PRACH and PUSCH packet processing
* rename read_prach_data() to xran_rx_prach_read_slot()
* in F release, there is no PRACH queue so `frame` and `slot` are
  passed as input; in K release, `frame` and `slot` are taken from the PRACH
queue

Note: since the PUSCH callback is the called before PRACH callback, this
commit is correct but the timing for PRACH processing is just a little
bit late. In previous state, PRACH processing was done inside the
function of PUSCH processing.
=> next commit will just reverse the order.

radio/fhi_72/oran_isolate.h
@@ -14,6 +14,8 @@
 #include "openair1/PHY/impl_defs_nr.h"
 #include "openair1/PHY/TOOLS/tools_defs.h"
 #include "openair1/PHY/defs_nr_common.h"
+#include "openair1/PHY/NR_TRANSPORT/nr_transport_proto.h"
+
 /*
  * Structure added to bear the information needed from OAI RU
  */
@@ -38,12 +40,18 @@ typedef struct ru_info_s {
   c16_t (*prach_buf)[NUMBER_OF_NR_RU_PRACH_OCCASIONS_MAX][NR_PRACH_SEQ_LEN_L];
 } ru_info_t;
 
-/** @brief Reads RX data (PRACH/PUSCH) of next slot.
+/** @brief Reads RX data PUSCH of next slot.
  *
  * @param ru pointer to structure keeping pointers to OAI data.
  * @param frame output of the frame which has been read.
  * @param slot output of the slot which has been read. */
 int xran_fh_rx_read_slot(ru_info_t *ru, int *frame, int *slot);
+/** @brief Reads RX data PRACH of next slot.
+ *
+ * @param ru pointer to structure keeping pointers to OAI data.
+ * @param frame input for F_RELEASE, output for K_RELEASE.
+ * @param slot input for F_RELEASE, output for K_RELEASE. */
+int xran_fh_rx_prach_read_slot(prach_list_t *prach_list, ru_info_t *ru, int *frame, int *slot);
 /** @brief Writes TX data (PDSCH) of given slot. */
 int xran_fh_tx_send_slot(ru_info_t *ru, int frame, int slot, uint64_t timestamp);
 

HarshaUndodi/Handover-simulation-networking-

Signed-off-by: Teodora Vladić <teodora.vladic@openairinterface.org>

radio/fhi_72/oran_isolate.h
@@ -14,6 +14,8 @@
 #include "openair1/PHY/impl_defs_nr.h"
 #include "openair1/PHY/TOOLS/tools_defs.h"
 #include "openair1/PHY/defs_nr_common.h"
+#include "openair1/PHY/NR_TRANSPORT/nr_transport_proto.h"
+
 /*
  * Structure added to bear the information needed from OAI RU
  */
@@ -38,12 +40,18 @@ typedef struct ru_info_s {
   c16_t (*prach_buf)[NUMBER_OF_NR_RU_PRACH_OCCASIONS_MAX][NR_PRACH_SEQ_LEN_L];
 } ru_info_t;
 
-/** @brief Reads RX data (PRACH/PUSCH) of next slot.
+/** @brief Reads RX data PUSCH of next slot.
  *
  * @param ru pointer to structure keeping pointers to OAI data.
  * @param frame output of the frame which has been read.
  * @param slot output of the slot which has been read. */
 int xran_fh_rx_read_slot(ru_info_t *ru, int *frame, int *slot);
+/** @brief Reads RX data PRACH of next slot.
+ *
+ * @param ru pointer to structure keeping pointers to OAI data.
+ * @param frame input for F_RELEASE, output for K_RELEASE.
+ * @param slot input for F_RELEASE, output for K_RELEASE. */
+int xran_fh_rx_prach_read_slot(PHY_VARS_gNB *gNB, ru_info_t *ru, int *frame, int *slot);
 /** @brief Writes TX data (PDSCH) of given slot. */
 int xran_fh_tx_send_slot(ru_info_t *ru, int frame, int slot, uint64_t timestamp);
 