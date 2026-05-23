Nyanm/oai-ran-tlab

fapi: add spatial steam mapping for MU-MIMO
Update copy functions and ctest.

nfapi/open-nFAPI/fapi/src/nr_fapi_p7_utils.c
@@ -1020,6 +1020,10 @@ static void copy_dl_tti_request_pdcch_pdu(const nfapi_nr_dl_tti_pdcch_pdu_rel15_
     copy_dl_tti_beamforming(&src_dci_pdu->precodingAndBeamforming, &dst_dci_pdu->precodingAndBeamforming);
     PARTIAL_COPY_RANGE(dst_dci_pdu, src_dci_pdu, nfapi_nr_dl_dci_pdu_t, beta_PDCCH_1_0, Payload);
   }
+  dst->param_v4.numSpatialStreams = src->param_v4.numSpatialStreams;
+  for (uint_fast16_t i = 0; i < dst->param_v4.numSpatialStreams; i++) {
+    dst->param_v4.dci_spatialStreamMap[i] = src->param_v4.dci_spatialStreamMap[i];
+  }
 }
 
 static void copy_dl_tti_request_pdsch_pdu(const nfapi_nr_dl_tti_pdsch_pdu_rel15_t *src, nfapi_nr_dl_tti_pdsch_pdu_rel15_t *dst)
@@ -1027,18 +1031,28 @@ static void copy_dl_tti_request_pdsch_pdu(const nfapi_nr_dl_tti_pdsch_pdu_rel15_
   PARTIAL_COPY(dst, src, nfapi_nr_dl_tti_pdsch_pdu_rel15_t, nEpreRatioOfPDSCHToPTRS);
   copy_dl_tti_beamforming(&src->precodingAndBeamforming, &dst->precodingAndBeamforming);
   PARTIAL_COPY_RANGE(dst, src, nfapi_nr_dl_tti_pdsch_pdu_rel15_t, powerControlOffset, maintenance_parms_v3);
+  dst->param_v4.numberCodewords = src->param_v4.numberCodewords;
+  for (uint_fast8_t i = 0; i < dst->param_v4.numberCodewords; i++) {
+    const nfapi_nr_spatial_stream_index_t *s = src->param_v4.spatialStreamsCw + i;
+    nfapi_nr_spatial_stream_index_t *d = dst->param_v4.spatialStreamsCw + i;
+    COPY_STRUCT_ARRAY(d, s, spatialStreamIndices, numSpatialStreamIndices);
+  }
 }
 
 static void copy_dl_tti_request_csi_rs_pdu(const nfapi_nr_dl_tti_csi_rs_pdu_rel15_t *src, nfapi_nr_dl_tti_csi_rs_pdu_rel15_t *dst)
 {
   PARTIAL_COPY(dst, src, nfapi_nr_dl_tti_csi_rs_pdu_rel15_t, power_control_offset_ss);
   copy_dl_tti_beamforming(&src->precodingAndBeamforming, &dst->precodingAndBeamforming);
+  const struct nfapi_nr_csi_spatial_stream_index *s = &src->param_v4;
+  struct nfapi_nr_csi_spatial_stream_index *d = &dst->param_v4;
+  COPY_STRUCT_ARRAY(d, s, spatialStreamIndices, numSpatialStreamIndices);
 }
 
 static void copy_dl_tti_request_ssb_pdu(const nfapi_nr_dl_tti_ssb_pdu_rel15_t *src, nfapi_nr_dl_tti_ssb_pdu_rel15_t *dst)
 {
   PARTIAL_COPY(dst, src, nfapi_nr_dl_tti_ssb_pdu_rel15_t, ssbRsrp);
   copy_dl_tti_beamforming(&src->precoding_and_beamforming, &dst->precoding_and_beamforming);
+  dst->param_v4 = src->param_v4;
 }
 
 static void copy_dl_tti_request_pdu(const nfapi_nr_dl_tti_request_pdu_t *src, nfapi_nr_dl_tti_request_pdu_t *dst)
@@ -1113,6 +1127,9 @@ static void copy_ul_tti_request_prach_pdu(const nfapi_nr_prach_pdu_t *src, nfapi
 {
   PARTIAL_COPY(dst, src, nfapi_nr_prach_pdu_t, num_cs);
   copy_ul_tti_beamforming(&src->beamforming, &dst->beamforming);
+  const nfapi_nr_spatial_stream_index_t *s = &src->param_v4;
+  nfapi_nr_spatial_stream_index_t *d = &dst->param_v4;
+  COPY_STRUCT_ARRAY(d, s, spatialStreamIndices, numSpatialStreamIndices);
 }
 
 static void copy_ul_tti_request_pusch_pdu(const nfapi_nr_pusch_pdu_t *src, nfapi_nr_pusch_pdu_t *dst)
@@ -1152,12 +1169,18 @@ static void copy_ul_tti_request_pusch_pdu(const nfapi_nr_pusch_pdu_t *src, nfapi
   copy_ul_tti_beamforming(&src->beamforming, &dst->beamforming);
   dst->maintenance_parms_v3.ldpcBaseGraph = src->maintenance_parms_v3.ldpcBaseGraph;
   dst->maintenance_parms_v3.tbSizeLbrmBytes = src->maintenance_parms_v3.tbSizeLbrmBytes;
+  const nfapi_nr_spatial_stream_index_t *s = &src->param_v4;
+  nfapi_nr_spatial_stream_index_t *d = &dst->param_v4;
+  COPY_STRUCT_ARRAY(d, s, spatialStreamIndices, numSpatialStreamIndices);
 }
 
 static void copy_ul_tti_request_pucch_pdu(const nfapi_nr_pucch_pdu_t *src, nfapi_nr_pucch_pdu_t *dst)
 {
   PARTIAL_COPY(dst, src, nfapi_nr_pucch_pdu_t, bit_len_csi_part2);
   copy_ul_tti_beamforming(&src->beamforming, &dst->beamforming);
+  const nfapi_nr_spatial_stream_index_t *s = &src->param_v4;
+  nfapi_nr_spatial_stream_index_t *d = &dst->param_v4;
+  COPY_STRUCT_ARRAY(d, s, spatialStreamIndices, numSpatialStreamIndices);
 }
 
 static void copy_ul_tti_request_srs_parameters(const nfapi_v4_srs_parameters_t *src,

nfapi/open-nFAPI/nfapi/public_inc/nfapi_nr_interface_scf.h
@@ -859,6 +859,21 @@ typedef struct {
 
 } nfapi_nr_dl_dci_pdu_t;
 
+// The maximum number of spatial streams to be mapped depends on TLV 0x16E and
+// the number of streams could be same as number of layers or number of antenna
+// ports or number of baseband ports. Hence we set this to be the maximum number
+// of baseband ports
+#define MAX_NUM_SPATIAL_STREAMS 16
+
+typedef struct {
+  uint16_t dci_index;
+  uint16_t spatial_stream_index;
+} nfapi_v4_dci_spatial_stream_index_t;
+
+typedef struct {
+  uint16_t numSpatialStreams;
+  nfapi_v4_dci_spatial_stream_index_t dci_spatialStreamMap[MAX_NUM_SPATIAL_STREAMS];
+} nfapi_v4_pdcch_pdu_parameters_t;
 
 typedef struct {
   ///Bandwidth part size [TS38.213 sec12]. Number of contiguous PRBs allocated to the BWP,Value: 1->275
@@ -891,12 +906,31 @@ typedef struct {
   uint16_t numDlDci;
   ///DL DCI PDU
   nfapi_nr_dl_dci_pdu_t dci_pdu[MAX_DCI_CORESET];
-}  nfapi_nr_dl_tti_pdcch_pdu_rel15_t;
+  /// Spatial stream indexing for MU-MIMO
+  nfapi_v4_pdcch_pdu_parameters_t param_v4;
+} nfapi_nr_dl_tti_pdcch_pdu_rel15_t;
 
 typedef struct {
   uint8_t ldpcBaseGraph;
   uint32_t tbSizeLbrmBytes;
 }nfapi_v3_pdsch_maintenance_parameters_t;
+
+typedef struct {
+  /// Number of spatial streams used in the index array
+  uint8_t numSpatialStreamIndices;
+  /// Spatial stream index array
+  uint16_t spatialStreamIndices[MAX_NUM_SPATIAL_STREAMS];
+} nfapi_nr_spatial_stream_index_t;
+
+#define MAX_NUM_CODEWORDS 2
+
+typedef struct {
+  // MU-MIMO support in FAPIv4
+  /// Number of codewords with spatial stream indices
+  uint8_t numberCodewords;
+  /// Spatial stream indexing for codeworeds
+  nfapi_nr_spatial_stream_index_t spatialStreamsCw[MAX_NUM_CODEWORDS];
+} nfapi_v4_pdsch_parameters_t;
 
 typedef struct {
   uint16_t pduBitmap;
@@ -990,8 +1024,9 @@ typedef struct {
   uint32_t dlTbCrc;
 
   nfapi_v3_pdsch_maintenance_parameters_t maintenance_parms_v3;
-}nfapi_nr_dl_tti_pdsch_pdu_rel15_t;
-
+  /// PDSCH parameters FAPI v4. used only for spatial stream indexing in MU-MIMO
+  nfapi_v4_pdsch_parameters_t param_v4;
+} nfapi_nr_dl_tti_pdsch_pdu_rel15_t;
 
 //for pdsch_pdu:
 /*
@@ -1075,8 +1110,14 @@ typedef struct
   uint8_t power_control_offset;     // Ratio of PDSCH EPRE to NZP CSI-RSEPRE [3GPP TS 38.214, sec 5.2.2.3.1], Value: 0->23 representing -8 to 15 dB in 1dB steps; 255: L1 is configured with ProfileSSS
   uint8_t power_control_offset_ss;  // Ratio of NZP CSI-RS EPRE to SSB/PBCH block EPRE [3GPP TS 38.214, sec 5.2.2.3.1], Values: 0: -3dB; 1: 0dB; 2: 3dB; 3: 6dB; 255: L1 is configured with ProfileSSS
   nfapi_nr_tx_precoding_and_beamforming_t precodingAndBeamforming;
+  /// Spatial stream indexing for MU-MIMO
+  struct nfapi_nr_csi_spatial_stream_index {
+    /// Number of spatial streams used in the index array
+    uint8_t numSpatialStreamIndices;
+    /// Spatial stream index array
+    uint8_t spatialStreamIndices[MAX_NUM_SPATIAL_STREAMS];
+  } param_v4;
 } nfapi_nr_dl_tti_csi_rs_pdu_rel15_t;
-
 
 typedef struct
 {
@@ -1118,6 +1159,11 @@ typedef struct {
   /// A value indicating the channel quality between the gNB and nrUE. Value: 0->255 dBM
   uint8_t  ssbRsrp;
   nfapi_nr_tx_precoding_and_beamforming_t precoding_and_beamforming;
+  /// Spatial stream indexing
+  struct nfapi_v4_ssb_param {
+    uint8_t spatialStreamIndexPresent;
+    uint16_t spatialStreamIndex;
+  } param_v4;
 } nfapi_nr_dl_tti_ssb_pdu_rel15_t;
 
 typedef struct {
@@ -1285,7 +1331,7 @@ typedef struct
   uint8_t  prach_start_symbol;
   uint16_t num_cs;
   nfapi_nr_ul_beamforming_t beamforming;
-
+  nfapi_nr_spatial_stream_index_t param_v4;
 } nfapi_nr_prach_pdu_t;
 
 //for pusch_pdu:
@@ -1398,6 +1444,8 @@ typedef struct
   //beamforming
   nfapi_nr_ul_beamforming_t beamforming;
   nfapi_v3_pdsch_maintenance_parameters_t maintenance_parms_v3;
+  // Spatial stream indexing for MU-MIMO
+  nfapi_nr_spatial_stream_index_t param_v4;
 } nfapi_nr_pusch_pdu_t;
 
 //for pucch_pdu:
@@ -1443,7 +1491,7 @@ typedef struct
   uint16_t bit_len_csi_part2;
 
   nfapi_nr_ul_beamforming_t beamforming;
-
+  nfapi_nr_spatial_stream_index_t param_v4;
 } nfapi_nr_pucch_pdu_t;
 
 typedef struct {

nfapi/tests/p7/nr_fapi_dl_tti_request_test.c
@@ -4,6 +4,7 @@
 #include "dci_payload_utils.h"
 #include "nr_fapi_p7.h"
 #include "nr_fapi_p7_utils.h"
+#include "nr_fapi_common_util_test.h"
 
 static void fill_dl_tti_request_beamforming(nfapi_nr_tx_precoding_and_beamforming_t *precodingAndBeamforming)
 {
@@ -47,6 +48,11 @@ static void fill_dl_tti_request_pdcch_pdu(nfapi_nr_dl_tti_pdcch_pdu_rel15_t *pdu
     pdu->dci_pdu[dl_dci].powerControlOffsetSS = rand8();
     pdu->dci_pdu[dl_dci].PayloadSizeBits = rand16_range(0, DCI_PAYLOAD_BYTE_LEN * 8);
     generate_payload(pdu->dci_pdu[dl_dci].PayloadSizeBits, pdu->dci_pdu[dl_dci].Payload);
+  }
+  pdu->param_v4.numSpatialStreams = get_num_spatial_streams();
+  for (int n = 0; n < pdu->param_v4.numSpatialStreams; n++) {
+    pdu->param_v4.dci_spatialStreamMap[n].dci_index = rand16();
+    pdu->param_v4.dci_spatialStreamMap[n].spatial_stream_index = rand16_range(0, pdu->param_v4.numSpatialStreams - 1);
   }
 }
 
@@ -109,6 +115,12 @@ static void fill_dl_tti_request_pdsch_pdu(nfapi_nr_dl_tti_pdsch_pdu_rel15_t *pdu
   }
   pdu->maintenance_parms_v3.ldpcBaseGraph = rand8();
   pdu->maintenance_parms_v3.tbSizeLbrmBytes = rand32();
+  pdu->param_v4.numberCodewords = rand8_range(0, MAX_NUM_CODEWORDS);
+  for (int c = 0; c < pdu->param_v4.numberCodewords; c++) {
+    pdu->param_v4.spatialStreamsCw[c].numSpatialStreamIndices = get_num_spatial_streams();
+    fill_spatial_streams(pdu->param_v4.spatialStreamsCw[c].numSpatialStreamIndices,
+                         pdu->param_v4.spatialStreamsCw[c].spatialStreamIndices);
+  }
 }
 
 static void fill_dl_tti_request_csi_rs_pdu(nfapi_nr_dl_tti_csi_rs_pdu_rel15_t *pdu)
@@ -147,6 +159,9 @@ static void fill_dl_tti_request_csi_rs_pdu(nfapi_nr_dl_tti_csi_rs_pdu_rel15_t *p
   pdu->power_control_offset = rand8_range(0, 23);
   pdu->power_control_offset_ss = rand8_range(0, 3);
   fill_dl_tti_request_beamforming(&pdu->precodingAndBeamforming);
+  pdu->param_v4.numSpatialStreamIndices = get_num_spatial_streams();
+  for (int n = 0; n < pdu->param_v4.numSpatialStreamIndices; n++)
+    pdu->param_v4.spatialStreamIndices[n] = rand8_range(0, pdu->param_v4.numSpatialStreamIndices - 1);
 }
 
 static void fill_dl_tti_request_ssb_pdu(nfapi_nr_dl_tti_ssb_pdu_rel15_t *pdu)
@@ -159,6 +174,8 @@ static void fill_dl_tti_request_ssb_pdu(nfapi_nr_dl_tti_ssb_pdu_rel15_t *pdu)
   pdu->bchPayloadFlag = rand8_range(0, 2);
   pdu->bchPayload = rand24();
   fill_dl_tti_request_beamforming(&pdu->precoding_and_beamforming);
+  pdu->param_v4.spatialStreamIndexPresent = rand8(0, 1);
+  pdu->param_v4.spatialStreamIndex = rand16_range(0, MAX_NUM_SPATIAL_STREAMS);
 }
 
 static void fill_dl_tti_request(nfapi_nr_dl_tti_request_t *msg)

HarshaUndodi/Handover-simulation-networking-

Signed-off-by: Sakthivel Velumani <s.velumani@northeastern.edu>

nfapi/open-nFAPI/fapi/src/nr_fapi_p7_utils.c
@@ -1020,6 +1020,10 @@ static void copy_dl_tti_request_pdcch_pdu(const nfapi_nr_dl_tti_pdcch_pdu_rel15_
     copy_dl_tti_beamforming(&src_dci_pdu->precodingAndBeamforming, &dst_dci_pdu->precodingAndBeamforming);
     PARTIAL_COPY_RANGE(dst_dci_pdu, src_dci_pdu, nfapi_nr_dl_dci_pdu_t, beta_PDCCH_1_0, Payload);
   }
+  dst->param_v4.numSpatialStreams = src->param_v4.numSpatialStreams;
+  for (uint_fast16_t i = 0; i < dst->param_v4.numSpatialStreams; i++) {
+    dst->param_v4.dci_spatialStreamIndices[i] = src->param_v4.dci_spatialStreamIndices[i];
+  }
 }
 
 static void copy_dl_tti_request_pdsch_pdu(const nfapi_nr_dl_tti_pdsch_pdu_rel15_t *src, nfapi_nr_dl_tti_pdsch_pdu_rel15_t *dst)
@@ -1027,18 +1031,28 @@ static void copy_dl_tti_request_pdsch_pdu(const nfapi_nr_dl_tti_pdsch_pdu_rel15_
   PARTIAL_COPY(dst, src, nfapi_nr_dl_tti_pdsch_pdu_rel15_t, nEpreRatioOfPDSCHToPTRS);
   copy_dl_tti_beamforming(&src->precodingAndBeamforming, &dst->precodingAndBeamforming);
   PARTIAL_COPY_RANGE(dst, src, nfapi_nr_dl_tti_pdsch_pdu_rel15_t, powerControlOffset, maintenance_parms_v3);
+  dst->param_v4.numberCodewords = src->param_v4.numberCodewords;
+  for (uint_fast8_t i = 0; i < dst->param_v4.numberCodewords; i++) {
+    const nfapi_nr_spatial_stream_index_t *s = src->param_v4.spatialStreamsCw + i;
+    nfapi_nr_spatial_stream_index_t *d = dst->param_v4.spatialStreamsCw + i;
+    COPY_STRUCT_ARRAY(d, s, spatialStreamIndices, numSpatialStreamIndices);
+  }
 }
 
 static void copy_dl_tti_request_csi_rs_pdu(const nfapi_nr_dl_tti_csi_rs_pdu_rel15_t *src, nfapi_nr_dl_tti_csi_rs_pdu_rel15_t *dst)
 {
   PARTIAL_COPY(dst, src, nfapi_nr_dl_tti_csi_rs_pdu_rel15_t, power_control_offset_ss);
   copy_dl_tti_beamforming(&src->precodingAndBeamforming, &dst->precodingAndBeamforming);
+  const struct nfapi_nr_csi_spatial_stream_index *s = &src->param_v4;
+  struct nfapi_nr_csi_spatial_stream_index *d = &dst->param_v4;
+  COPY_STRUCT_ARRAY(d, s, spatialStreamIndices, numSpatialStreamIndices);
 }
 
 static void copy_dl_tti_request_ssb_pdu(const nfapi_nr_dl_tti_ssb_pdu_rel15_t *src, nfapi_nr_dl_tti_ssb_pdu_rel15_t *dst)
 {
   PARTIAL_COPY(dst, src, nfapi_nr_dl_tti_ssb_pdu_rel15_t, ssbRsrp);
   copy_dl_tti_beamforming(&src->precoding_and_beamforming, &dst->precoding_and_beamforming);
+  dst->param_v4 = src->param_v4;
 }
 
 static void copy_dl_tti_request_pdu(const nfapi_nr_dl_tti_request_pdu_t *src, nfapi_nr_dl_tti_request_pdu_t *dst)
@@ -1113,6 +1127,9 @@ static void copy_ul_tti_request_prach_pdu(const nfapi_nr_prach_pdu_t *src, nfapi
 {
   PARTIAL_COPY(dst, src, nfapi_nr_prach_pdu_t, num_cs);
   copy_ul_tti_beamforming(&src->beamforming, &dst->beamforming);
+  const nfapi_nr_spatial_stream_index_t *s = &src->param_v4;
+  nfapi_nr_spatial_stream_index_t *d = &dst->param_v4;
+  COPY_STRUCT_ARRAY(d, s, spatialStreamIndices, numSpatialStreamIndices);
 }
 
 static void copy_ul_tti_request_pusch_pdu(const nfapi_nr_pusch_pdu_t *src, nfapi_nr_pusch_pdu_t *dst)
@@ -1152,12 +1169,18 @@ static void copy_ul_tti_request_pusch_pdu(const nfapi_nr_pusch_pdu_t *src, nfapi
   copy_ul_tti_beamforming(&src->beamforming, &dst->beamforming);
   dst->maintenance_parms_v3.ldpcBaseGraph = src->maintenance_parms_v3.ldpcBaseGraph;
   dst->maintenance_parms_v3.tbSizeLbrmBytes = src->maintenance_parms_v3.tbSizeLbrmBytes;
+  const nfapi_nr_spatial_stream_index_t *s = &src->param_v4;
+  nfapi_nr_spatial_stream_index_t *d = &dst->param_v4;
+  COPY_STRUCT_ARRAY(d, s, spatialStreamIndices, numSpatialStreamIndices);
 }
 
 static void copy_ul_tti_request_pucch_pdu(const nfapi_nr_pucch_pdu_t *src, nfapi_nr_pucch_pdu_t *dst)
 {
   PARTIAL_COPY(dst, src, nfapi_nr_pucch_pdu_t, bit_len_csi_part2);
   copy_ul_tti_beamforming(&src->beamforming, &dst->beamforming);
+  const nfapi_nr_spatial_stream_index_t *s = &src->param_v4;
+  nfapi_nr_spatial_stream_index_t *d = &dst->param_v4;
+  COPY_STRUCT_ARRAY(d, s, spatialStreamIndices, numSpatialStreamIndices);
 }
 
 static void copy_ul_tti_request_srs_parameters(const nfapi_v4_srs_parameters_t *src,

nfapi/open-nFAPI/nfapi/public_inc/nfapi_nr_interface_scf.h
@@ -853,6 +853,21 @@ typedef struct {
 
 } nfapi_nr_dl_dci_pdu_t;
 
+// The maximum number of spatial streams to be mapped depends on TLV 0x16E and
+// the number of streams could be same as number of layers or number of antenna
+// ports or number of baseband ports. Hence we set this to be the maximum number
+// of baseband ports
+#define MAX_NUM_SPATIAL_STREAMS 16
+
+typedef struct {
+  uint16_t dci_index;
+  uint16_t spatial_stream_index;
+} nfapi_v4_dci_spatial_stream_index_t;
+
+typedef struct {
+  uint16_t numSpatialStreams;
+  nfapi_v4_dci_spatial_stream_index_t dci_spatialStreamIndices[MAX_NUM_SPATIAL_STREAMS];
+} nfapi_v4_pdcch_pdu_parameters_t;
 
 typedef struct {
   ///Bandwidth part size [TS38.213 sec12]. Number of contiguous PRBs allocated to the BWP,Value: 1->275
@@ -885,12 +900,31 @@ typedef struct {
   uint16_t numDlDci;
   ///DL DCI PDU
   nfapi_nr_dl_dci_pdu_t dci_pdu[MAX_DCI_CORESET];
-}  nfapi_nr_dl_tti_pdcch_pdu_rel15_t;
+  /// Spatial stream indexing for MU-MIMO
+  nfapi_v4_pdcch_pdu_parameters_t param_v4;
+} nfapi_nr_dl_tti_pdcch_pdu_rel15_t;
 
 typedef struct {
   uint8_t ldpcBaseGraph;
   uint32_t tbSizeLbrmBytes;
 }nfapi_v3_pdsch_maintenance_parameters_t;
+
+typedef struct {
+  /// Number of spatial streams used in the index array
+  uint8_t numSpatialStreamIndices;
+  /// Spatial stream index array
+  uint16_t spatialStreamIndices[MAX_NUM_SPATIAL_STREAMS];
+} nfapi_nr_spatial_stream_index_t;
+
+#define MAX_NUM_CODEWORDS 2
+
+typedef struct {
+  // MU-MIMO support in FAPIv4
+  /// Number of codewords with spatial stream indices
+  uint8_t numberCodewords;
+  /// Spatial stream indexing for codeworeds
+  nfapi_nr_spatial_stream_index_t spatialStreamsCw[MAX_NUM_CODEWORDS];
+} nfapi_v4_pdsch_parameters_t;
 
 typedef struct {
   uint16_t pduBitmap;
@@ -984,8 +1018,9 @@ typedef struct {
   uint32_t dlTbCrc;
 
   nfapi_v3_pdsch_maintenance_parameters_t maintenance_parms_v3;
-}nfapi_nr_dl_tti_pdsch_pdu_rel15_t;
-
+  /// PDSCH parameters FAPI v4. used only for spatial stream indexing in MU-MIMO
+  nfapi_v4_pdsch_parameters_t param_v4;
+} nfapi_nr_dl_tti_pdsch_pdu_rel15_t;
 
 //for pdsch_pdu:
 /*
@@ -1069,8 +1104,14 @@ typedef struct
   uint8_t power_control_offset;     // Ratio of PDSCH EPRE to NZP CSI-RSEPRE [3GPP TS 38.214, sec 5.2.2.3.1], Value: 0->23 representing -8 to 15 dB in 1dB steps; 255: L1 is configured with ProfileSSS
   uint8_t power_control_offset_ss;  // Ratio of NZP CSI-RS EPRE to SSB/PBCH block EPRE [3GPP TS 38.214, sec 5.2.2.3.1], Values: 0: -3dB; 1: 0dB; 2: 3dB; 3: 6dB; 255: L1 is configured with ProfileSSS
   nfapi_nr_tx_precoding_and_beamforming_t precodingAndBeamforming;
+  /// Spatial stream indexing for MU-MIMO
+  struct nfapi_nr_csi_spatial_stream_index {
+    /// Number of spatial streams used in the index array
+    uint8_t numSpatialStreamIndices;
+    /// Spatial stream index array
+    uint8_t spatialStreamIndices[MAX_NUM_SPATIAL_STREAMS];
+  } param_v4;
 } nfapi_nr_dl_tti_csi_rs_pdu_rel15_t;
-
 
 typedef struct
 {
@@ -1112,6 +1153,11 @@ typedef struct {
   /// A value indicating the channel quality between the gNB and nrUE. Value: 0->255 dBM
   uint8_t  ssbRsrp;
   nfapi_nr_tx_precoding_and_beamforming_t precoding_and_beamforming;
+  /// Spatial stream indexing
+  struct nfapi_v4_ssb_param {
+    uint8_t spatialStreamIndexPresent;
+    uint16_t spatialStreamIndex;
+  } param_v4;
 } nfapi_nr_dl_tti_ssb_pdu_rel15_t;
 
 typedef struct {
@@ -1279,7 +1325,7 @@ typedef struct
   uint8_t  prach_start_symbol;
   uint16_t num_cs;
   nfapi_nr_ul_beamforming_t beamforming;
-
+  nfapi_nr_spatial_stream_index_t param_v4;
 } nfapi_nr_prach_pdu_t;
 
 //for pusch_pdu:
@@ -1392,6 +1438,8 @@ typedef struct
   //beamforming
   nfapi_nr_ul_beamforming_t beamforming;
   nfapi_v3_pdsch_maintenance_parameters_t maintenance_parms_v3;
+  // Spatial stream indexing for MU-MIMO
+  nfapi_nr_spatial_stream_index_t param_v4;
 } nfapi_nr_pusch_pdu_t;
 
 //for pucch_pdu:
@@ -1437,7 +1485,7 @@ typedef struct
   uint16_t bit_len_csi_part2;
 
   nfapi_nr_ul_beamforming_t beamforming;
-
+  nfapi_nr_spatial_stream_index_t param_v4;
 } nfapi_nr_pucch_pdu_t;
 
 typedef struct {

nfapi/tests/p7/nr_fapi_dl_tti_request_test.c
@@ -4,6 +4,7 @@
 #include "dci_payload_utils.h"
 #include "nr_fapi_p7.h"
 #include "nr_fapi_p7_utils.h"
+#include "nr_fapi_common_util_test.h"
 
 static void fill_dl_tti_request_beamforming(nfapi_nr_tx_precoding_and_beamforming_t *precodingAndBeamforming)
 {
@@ -47,6 +48,11 @@ static void fill_dl_tti_request_pdcch_pdu(nfapi_nr_dl_tti_pdcch_pdu_rel15_t *pdu
     pdu->dci_pdu[dl_dci].powerControlOffsetSS = rand8();
     pdu->dci_pdu[dl_dci].PayloadSizeBits = rand16_range(0, DCI_PAYLOAD_BYTE_LEN * 8);
     generate_payload(pdu->dci_pdu[dl_dci].PayloadSizeBits, pdu->dci_pdu[dl_dci].Payload);
+  }
+  pdu->param_v4.numSpatialStreams = get_num_spatial_streams();
+  for (int n = 0; n < pdu->param_v4.numSpatialStreams; n++) {
+    pdu->param_v4.dci_spatialStreamIndices[n].dci_index = rand16();
+    pdu->param_v4.dci_spatialStreamIndices[n].spatial_stream_index = rand16_range(0, pdu->param_v4.numSpatialStreams - 1);
   }
 }
 
@@ -109,6 +115,12 @@ static void fill_dl_tti_request_pdsch_pdu(nfapi_nr_dl_tti_pdsch_pdu_rel15_t *pdu
   }
   pdu->maintenance_parms_v3.ldpcBaseGraph = rand8();
   pdu->maintenance_parms_v3.tbSizeLbrmBytes = rand32();
+  pdu->param_v4.numberCodewords = rand8_range(0, MAX_NUM_CODEWORDS);
+  for (int c = 0; c < pdu->param_v4.numberCodewords; c++) {
+    pdu->param_v4.spatialStreamsCw[c].numSpatialStreamIndices = get_num_spatial_streams();
+    fill_spatial_streams(pdu->param_v4.spatialStreamsCw[c].numSpatialStreamIndices,
+                         pdu->param_v4.spatialStreamsCw[c].spatialStreamIndices);
+  }
 }
 
 static void fill_dl_tti_request_csi_rs_pdu(nfapi_nr_dl_tti_csi_rs_pdu_rel15_t *pdu)
@@ -147,6 +159,9 @@ static void fill_dl_tti_request_csi_rs_pdu(nfapi_nr_dl_tti_csi_rs_pdu_rel15_t *p
   pdu->power_control_offset = rand8_range(0, 23);
   pdu->power_control_offset_ss = rand8_range(0, 3);
   fill_dl_tti_request_beamforming(&pdu->precodingAndBeamforming);
+  pdu->param_v4.numSpatialStreamIndices = get_num_spatial_streams();
+  for (int n = 0; n < pdu->param_v4.numSpatialStreamIndices; n++)
+    pdu->param_v4.spatialStreamIndices[n] = rand8_range(0, pdu->param_v4.numSpatialStreamIndices - 1);
 }
 
 static void fill_dl_tti_request_ssb_pdu(nfapi_nr_dl_tti_ssb_pdu_rel15_t *pdu)
@@ -159,6 +174,8 @@ static void fill_dl_tti_request_ssb_pdu(nfapi_nr_dl_tti_ssb_pdu_rel15_t *pdu)
   pdu->bchPayloadFlag = rand8_range(0, 2);
   pdu->bchPayload = rand24();
   fill_dl_tti_request_beamforming(&pdu->precoding_and_beamforming);
+  pdu->param_v4.spatialStreamIndexPresent = rand8(0, 1);
+  pdu->param_v4.spatialStreamIndex = rand16_range(0, MAX_NUM_SPATIAL_STREAMS);
 }
 
 static void fill_dl_tti_request(nfapi_nr_dl_tti_request_t *msg)